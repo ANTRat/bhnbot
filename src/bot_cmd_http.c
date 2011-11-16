5cced34807cf30b21e48be18773ff4a81b88ef34
Jonathan Works <jgworks@gmail.com>
2011-11-16 17:52:59 -0500

    all urls are added to a sqlite3 db and are looked up before they are curl'ed and a message is printed if the link has been posted before.

--- src/bot_cmd_http.c
+++ src/bot_cmd_http.c
@@ -17,7 +17,8 @@
 #include <sqlite3.h>
 
 sqlite3* db;
-sqlite3_stmt* stmt;
+sqlite3_stmt* ins_stmt;
+sqlite3_stmt* srch_stmt;
 
 #endif
 
@@ -79,73 +80,91 @@ size_t cmd_http_writecallback(char *ptr, size_t size, size_t nmemb, void *userda
 
 int cmd_http(int s, int https, char* line, char* token) {
     int status = 0;
-    title = malloc(sizeof(char) * 4096);
-    memset(title, 0,  4096);
-
-    CURL* c = curl_easy_init();
-    if( https ) {
-        curl_easy_setopt(c, CURLOPT_URL, token + strlen("https://"));
-    } else {
-        curl_easy_setopt(c, CURLOPT_URL, token + strlen("http://"));
-    }
-    curl_easy_setopt(c, CURLOPT_PROTOCOLS, CURLPROTO_HTTP);
-    curl_easy_setopt(c, CURLOPT_REDIR_PROTOCOLS, CURLPROTO_HTTP);
-    curl_easy_setopt(c, CURLOPT_HTTPGET, 1);
-    curl_easy_setopt(c, CURLOPT_FOLLOWLOCATION, 1);
-    curl_easy_setopt(c, CURLOPT_MAXREDIRS, 10);
-    //curl_easy_setopt(c, CURLOPT_USERAGENT, "curl/7.21.3 (x86_64-pc-linux-gnu) libcurl/7.21.3 OpenSSL/0.9.8o zlib/1.2.3.4 libidn/1.18");
-    curl_easy_setopt(c, CURLOPT_WRITEFUNCTION, &cmd_http_writecallback);
-    curl_easy_setopt(c, CURLOPT_VERBOSE, 1);
-    curl_easy_perform(c);
-
-
-    long resp;
-    char* url;
-    curl_easy_getinfo(c, CURLINFO_RESPONSE_CODE, &resp);
-    curl_easy_getinfo(c, CURLINFO_EFFECTIVE_URL, &url);
-
+    int found = 0;
     char* pong_msg = malloc(sizeof(char) * 4096);
     memset(pong_msg, 0,  4096);
+#ifdef HAVE_LIBSQLITE3
+    int rc;
+    const char* prev_nick;
+    const char* prev_line;
+    sqlite3_bind_text(srch_stmt, 1, token, -1, SQLITE_TRANSIENT);
+    while((rc = sqlite3_step(srch_stmt)) == SQLITE_ROW) {
+        found++;
+        prev_nick = sqlite3_column_text(srch_stmt, 0);
+        prev_line = sqlite3_column_text(srch_stmt, 4);
+        break;
+    }
+    if( found ) {
+        sprintf(pong_msg, "PRIVMSG %s :[ OFN :: <%s> %s ]\r\n", IRC_CHANNEL, prev_nick, prev_line );
+    }
+    sqlite3_reset(srch_stmt);
+#endif
+    if( !found ) {
+        title = malloc(sizeof(char) * 4096);
+        memset(title, 0,  4096);
+
+        CURL* c = curl_easy_init();
+        if( https ) {
+            curl_easy_setopt(c, CURLOPT_URL, token + strlen("https://"));
+        } else {
+            curl_easy_setopt(c, CURLOPT_URL, token + strlen("http://"));
+        }
+        curl_easy_setopt(c, CURLOPT_PROTOCOLS, CURLPROTO_HTTP);
+        curl_easy_setopt(c, CURLOPT_REDIR_PROTOCOLS, CURLPROTO_HTTP);
+        curl_easy_setopt(c, CURLOPT_HTTPGET, 1);
+        curl_easy_setopt(c, CURLOPT_FOLLOWLOCATION, 1);
+        curl_easy_setopt(c, CURLOPT_MAXREDIRS, 10);
+        //curl_easy_setopt(c, CURLOPT_USERAGENT, "curl/7.21.3 (x86_64-pc-linux-gnu) libcurl/7.21.3 OpenSSL/0.9.8o zlib/1.2.3.4 libidn/1.18");
+        curl_easy_setopt(c, CURLOPT_WRITEFUNCTION, &cmd_http_writecallback);
+        curl_easy_setopt(c, CURLOPT_VERBOSE, 1);
+        curl_easy_perform(c);
+
+
+        long resp;
+        char* url;
+        curl_easy_getinfo(c, CURLINFO_RESPONSE_CODE, &resp);
+        curl_easy_getinfo(c, CURLINFO_EFFECTIVE_URL, &url);
 
 #ifdef HAVE_LIBSQLITE3
-    char* nick = strdup(line + 1);
-    char* start_line = strstr(nick, ":") + 1;
-    char* end_nick = strstr(nick, "!");
-    end_nick[0] = '\0';
-    int rc;
-    rc = sqlite3_bind_text(stmt, 1, nick, -1, SQLITE_TRANSIENT);
-    rc = sqlite3_bind_text(stmt, 2, token, -1, SQLITE_TRANSIENT);
-    rc = sqlite3_bind_int(stmt, 3, resp);
-    if( strlen(title) > 0 ) {
-        rc = sqlite3_bind_text(stmt, 4, title, -1, SQLITE_TRANSIENT);
-    } else {
-        rc = sqlite3_bind_text(stmt, 4, "", 0, SQLITE_STATIC);
-    }
-    rc = sqlite3_bind_text(stmt, 5, start_line, -1, SQLITE_TRANSIENT);
-    rc = sqlite3_step(stmt);
-    if( rc != SQLITE_DONE ) {
-        fprintf(stderr, "sqlite3_step() failed: %i\n", rc);
-    }
-    sqlite3_reset(stmt);
-    free(nick);
+        char* nick = strdup(line + 1);
+        char* start_line = strstr(nick, ":") + 1;
+        char* end_nick = strstr(nick, "!");
+        end_nick[0] = '\0';
+        rc = sqlite3_bind_text(ins_stmt, 1, nick, -1, SQLITE_TRANSIENT);
+        rc = sqlite3_bind_text(ins_stmt, 2, token, -1, SQLITE_TRANSIENT);
+        rc = sqlite3_bind_int(ins_stmt, 3, resp);
+        if( strlen(title) > 0 ) {
+            rc = sqlite3_bind_text(ins_stmt, 4, title, -1, SQLITE_TRANSIENT);
+        } else {
+            rc = sqlite3_bind_text(ins_stmt, 4, "", 0, SQLITE_STATIC);
+        }
+        rc = sqlite3_bind_text(ins_stmt, 5, start_line, -1, SQLITE_TRANSIENT);
+        rc = sqlite3_step(ins_stmt);
+        if( rc != SQLITE_DONE ) {
+            fprintf(stderr, "sqlite3_step() failed: %i\n", rc);
+        }
+        sqlite3_reset(ins_stmt);
+        free(nick);
 #endif
 
-    switch(resp) {
-        case 200:
-            if( strlen(title) > 0 ) {
-                sprintf(pong_msg, "PRIVMSG %s :[ %s ]\r\n", IRC_CHANNEL, title );
-            }
-            break;
-        default:
-            sprintf(pong_msg, "PRIVMSG %s :[ HttpErr: %li ]\r\n", IRC_CHANNEL, resp );
-            break;
+        switch(resp) {
+            case 200:
+                if( strlen(title) > 0 ) {
+                    sprintf(pong_msg, "PRIVMSG %s :[ %s ]\r\n", IRC_CHANNEL, title );
+                }
+                break;
+            default:
+                sprintf(pong_msg, "PRIVMSG %s :[ HttpErr: %li ]\r\n", IRC_CHANNEL, resp );
+                break;
+        }
+
+        free(title);
+        curl_easy_cleanup(c);
     }
-
-    send(s, pong_msg, strlen(pong_msg), 0);
-
+    if( strlen(pong_msg) > 0 ) {
+        send(s, pong_msg, strlen(pong_msg), 0);
+    }
     free(pong_msg);
-    free(title);
-    curl_easy_cleanup(c);
     return status;
 }
 
@@ -163,10 +182,17 @@ void cmd_http_init() {
         return;
     }
 
-    char* stmt_sql = "insert into http_urls (nick, url, resp, title, line) values (?, ?, ?, ?, ?);";
-    rc = sqlite3_prepare_v2( db, stmt_sql, strlen(stmt_sql), &stmt, NULL);
+    char* ins_stmt_sql = "insert into http_urls (nick, url, resp, title, line) values (?, ?, ?, ?, ?);";
+    rc = sqlite3_prepare_v2( db, ins_stmt_sql, strlen(ins_stmt_sql), &ins_stmt, NULL);
     if( rc != SQLITE_OK ) {
-        fprintf(stderr, "prepare failed: %i\n", rc);
+        fprintf(stderr, "prepare ins_stmt failed: %i\n", rc);
+        return;
+    }
+
+    char* srch_stmt_sql = "select nick, url, resp, title, line from http_urls where url = ?;";
+    rc = sqlite3_prepare_v2( db, srch_stmt_sql, strlen(srch_stmt_sql), &srch_stmt, NULL);
+    if( rc != SQLITE_OK ) {
+        fprintf(stderr, "prepare srch_stmt failed: %i\n", rc);
         return;
     }
 #endif
@@ -177,7 +203,8 @@ void cmd_http_cleanup() {
     curl_global_cleanup();
 
 #ifdef HAVE_LIBSQLITE3
-    sqlite3_finalize(stmt);
+    sqlite3_finalize(ins_stmt);
+    sqlite3_finalize(srch_stmt);
     sqlite3_close(db);
 #endif
 
